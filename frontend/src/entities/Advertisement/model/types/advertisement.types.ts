5ab5459fa09c1729ff98cc6aa66ba70161b100d0
Ruslan <cmetall@rambler.ru>
2025-11-22 22:39:29 +0300

    relocate ModerationHistory from entity Moderator to Advertisement

--- frontend/src/entities/Advertisement/model/types/advertisement.types.ts
+++ frontend/src/entities/Advertisement/model/types/advertisement.types.ts
@@ -1,5 +1,9 @@
 import type { Category } from "../../../../shared/types/categories.types";
+import type { Color } from "../../../../shared/types/color.types";
+import type { ConditionStatus } from "../../../../shared/types/conditionStatus.types";
+import type { GuaranteeStatus } from "../../../../shared/types/guaranteeStatus.types";
 import type { PriorityStatus } from "../../../../shared/types/priorityStatus.types";
+import type { RejectionReason } from "../../../../shared/types/rejectionReason.types";
 import type { Status } from "../../../../shared/types/status.types";
 import type { Seller } from "../../../Seller";
 
@@ -16,6 +20,26 @@ export interface Advertisement {
     updatedAt: string,
     images: string[], 
     seller: Seller,
-    characteristics: Record<string, string>,
-    moderationHistory: string[],
+    characteristics: Characteristics,
+    moderationHistory: ModerationHistory[],
 }
+
+interface Characteristics {
+    'Состояние': ConditionStatus,
+    'Гарантия': GuaranteeStatus,
+    'Производитель': `Бренд ${string}`,
+    'Модель': `Модель ${number}`,
+    'Цвет': Color,
+}
+
+interface ModerationHistory {
+    id: number,
+    moderatorId: number,
+    moderatorName: string,
+    action: Status,
+    reason: RejectionReason | null,
+    comment: Comment,
+    timestamp: string,
+}
+
+type Comment = 'Объявление не соответствует правилам платформы' | 'Объявление прошло модерацию успешно'
