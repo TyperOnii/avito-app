30a1c34cc5efa14579783b8ec8f44a03bc2efe1e
Ruslan <cmetall@rambler.ru>
2025-11-23 01:46:33 +0300

    refactoring aliases and imports

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,10 +1,11 @@
 import { ROUTES } from '@shared/config/routes'
-import { AdvertisementByIdPage } from 'pages/AdvertisementByIdPage/AdvertisementByIdPage'
-import { MainPage } from 'pages/MainPage/MainPage'
-import { StatsPage } from 'pages/StatsPage/StatsPage'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import { Layout } from './Layout'
 import { ThemeProvider } from './providers/ThemeProvider/ThemeProvider'
+import { AdvertisementByIdPage } from '@pages/AdvertisementByIdPage/AdvertisementByIdPage'
+import { MainPage } from '@pages/MainPage/MainPage'
+import { StatsPage } from '@pages/StatsPage/StatsPage'
+
 
 export const App = () => {
   return (
